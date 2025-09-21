1be53e4f6647711babe56716eb84e81616e57a8b
isholah360 <isholah360@gmail.com>
2025-09-21 13:42:05 -0700

    dash-img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,8 @@
-
 import React, { useState } from "react";
 
 import DashboardCard from "./DashboardCard";
 
 import ProjectTable from "./ProjectTable";
-
 
 import Officer from "./Officer";
 
@@ -12,60 +10,57 @@ function Home() {
   const [activeTab, setActiveTab] = useState("Dashboard");
   return (
     <div className="flex min-h-screen bg-gray-50 font-sans">
-        <main className="flex-1 p-6">
-          {activeTab === "Dashboard" && (
-            <>
-              <div className="pb-0 h-[30%]">
-                <img
-                  className="rounded-2xl h-[80%] w-full"
-                  src="https://wallpapers.com/images/hd/agriculture-background-oiknoaw5ndsbb98o.jpg"
-                  alt=""
-                />
-              </div>
+      <main className="flex-1 p-6">
+        {activeTab === "Dashboard" && (
+          <>
+            <div className="pb-0 h-[15rem] mb-10">
+              <img
+                className="rounded-2xl w-full h-[100%] object-cover"
+                src="https://wallpapers.com/images/hd/agriculture-background-oiknoaw5ndsbb98o.jpg"
+                alt=""
+              />
+            </div>
 
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 mb-8">
-                <DashboardCard
-                  title="Number of Officers"
-                  subtitle="..."
-                  value="18"
-                  emoji="👮‍♂️"
-                  color="blue"
-                />
-                <DashboardCard
-                  title="Number of Farmers"
-                  subtitle="..."
-                  value="18"
-                  emoji="👨‍🌾"
-                  color="indigo"
-                />
-                <DashboardCard
-                  title="Number of Farms"
-                  subtitle="..."
-                  value="18"
-                  emoji="🚜"
-                  color="green"
-                />
-                <DashboardCard
-                  title="Number of Livestock"
-                  subtitle="..."
-                  value="18"
-                  emoji="🐄"
-                  color="purple"
-                />
-              </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 mb-8 sm">
+              <DashboardCard
+                title="Number of Officers"
+                subtitle="..."
+                value="18"
+                emoji="👮‍♂️"
+                color="blue"
+              />
+              <DashboardCard
+                title="Number of Farmers"
+                subtitle="..."
+                value="18"
+                emoji="👨‍🌾"
+                color="indigo"
+              />
+              <DashboardCard
+                title="Number of Farms"
+                subtitle="..."
+                value="18"
+                emoji="🚜"
+                color="green"
+              />
+              <DashboardCard
+                title="Number of Livestock"
+                subtitle="..."
+                value="18"
+                emoji="🐄"
+                color="purple"
+              />
+            </div>
 
-              <div className="bg-white rounded-xl shadow-sm border border-gray-200 p-5">
-                <h3 className="text-lg font-semibold text-gray-800 mb-4">
-                  Officers 
-                </h3>
-                <ProjectTable />
-              </div>
-              
-            </>
-          )}
-
-          
-        </main>
+            <div className="bg-white rounded-xl shadow-sm border border-gray-200 p-5">
+              <h3 className="text-lg font-semibold text-gray-800 mb-4">
+                Officers
+              </h3>
+              <ProjectTable />
+            </div>
+          </>
+        )}
+      </main>
     </div>
   );
 }
