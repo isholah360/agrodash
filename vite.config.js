55ae51a28f2ffa23df1d0ec1034bf6211b73113c
isholah360 <isholah360@gmail.com>
2025-10-05 08:55:15 -0700

    vite-config-build

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,16 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+   build: {
+    chunkSizeWarningLimit: 2000, 
+  },
   server: {
     proxy: {
       '/v1': {
         // target: 'https://oyo-agri-backend-production.up.railway.app',
         target:"http://localhost:5000",
         changeOrigin: true,
-        secure: false,
+        secure: false,gti
         rewrite: (path) => path.replace(/^\/api/, '/api')
       },
       '/api': {
