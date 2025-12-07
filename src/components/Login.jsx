4b96f8c58459dac1b007d0f491fe647074f84d2c
isholah360 <isholah360@gmail.com>
2025-12-07 12:27:54 -0800

    anotherlogincorrected

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
       const encodedPassword = encodeURIComponent(password);
 
       const response = await fetch(
-        `/User/login?userName=${encodedUserName}&password=${encodedPassword}`,
+        `api/v1/User/login?userName=${encodedUserName}&password=${encodedPassword}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
